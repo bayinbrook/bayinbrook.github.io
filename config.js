7db9b04e1f0c0baa459a987cf5ab77026fa9342c
bayinbrook <bayinbrook@comeyou.com>
2026-05-13 18:32:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,8 @@
 // 留言板配置文件
-// 使用 GitHub Issues 存储留言，无需 Token
-
 const CONFIG = {
-  // GitHub 仓库信息
   OWNER: "bayinbrook",
   REPO: "bayinbrook.github.io",
-
-  // GitHub Pages 基础URL
   BASE_URL: "https://bayinbrook.github.io/",
-
-  // 每页显示留言数量
   PAGE_SIZE: 20
 };
-
 const API_BASE = "https://api.github.com";
